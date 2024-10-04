49c81fe2ca3977a47722d7615b3111845679c1b9
Alex Cantón <alexcantongarcia97@gmail.com>
2024-10-04 19:34:54 +0200

    Add error handler

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "./entities/user.entity";
 import { Model } from "mongoose";
@@ -10,14 +10,22 @@ export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) { }
 
   async create(createUserDto: CreateUserDto) {
-    const newUser = {
-      ...createUserDto,
-      auth0Id: createUserDto.user_id,
-      createdAt: createUserDto.created_at,
-      updatedAt: createUserDto.updated_at,
+    try {
+      const newUser = {
+        ...createUserDto,
+        auth0Id: createUserDto.user_id,
+        createdAt: createUserDto.created_at,
+        updatedAt: createUserDto.updated_at,
+      }
+      await this.userModel.create(newUser);
+      return newUser;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`User with email <${createUserDto.email}> is already exists.`);
+      }
+
+      throw new InternalServerErrorException(error.message);
     }
-    await this.userModel.create(newUser);
-    return newUser;
   }
 
   async findAll() {
