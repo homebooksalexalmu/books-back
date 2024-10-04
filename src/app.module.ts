3a85559d5b53fe3983780529cbdb8923612d229e
Alex Cantón <alexcantongarcia97@gmail.com>
2024-10-04 18:08:04 +0200

    Add MongooseModule to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot()],
+  imports: [UsersModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
