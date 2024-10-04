092ac52eed095acf747cbf5ad9c414516a2aabf7
Alex Cantón <alexcantongarcia97@gmail.com>
2024-10-04 18:30:58 +0200

    Modify code to get from database

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { User } from "./entities/user.entity";
+import { Model } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
 
 @Injectable()
 export class UsersService {
+
+  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+
   create(createUserDto: CreateUserDto) {
     return "This action adds a new user";
   }
 
-  findAll() {
-    return `This action returns all users: ${process.env.MONGODB_URI}`;
+  async findAll() {
+    const result = await this.userModel.find({});
+    console.log(result)
+    return JSON.stringify(result);
   }
 }
