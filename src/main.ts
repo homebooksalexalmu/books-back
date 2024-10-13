aff4b4df54f5f2f7a92e317e2086d0438a6eecdb
Alex Cantón <alexcantongarcia97@gmail.com>
2024-10-13 21:06:49 +0200

    Changes in cors

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,21 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true
+  });
 
+  app.setGlobalPrefix("api");
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+  }));
   app.enableCors({
     origin: ["http://localhost:3001", "https://books-frontend-rho.vercel.app/"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-  app.setGlobalPrefix("api");
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-  }));
 
   await app.listen(3000);
 }
