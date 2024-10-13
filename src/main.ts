0a860380d30b1a16876de749b465263d764d3b7a
Alex Cantón <alexcantongarcia97@gmail.com>
2024-10-13 20:37:43 +0200

    Changes in cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
